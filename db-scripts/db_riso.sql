9695328ee08252676792b47aa6afcd4985213b45
igormeira <igor.meira@gmail.com>
2017-08-30 16:47:34 -0300

    Colocando dados no BD

--- db-scripts/db_riso.sql
+++ db-scripts/db_riso.sql
@@ -14,13 +14,13 @@ CREATE SCHEMA IF NOT EXISTS `RISO` DEFAULT CHARACTER SET utf8 ;
 USE `RISO` ;
 
 -- -----------------------------------------------------
--- Table `RISO`.`tb_termo`
+-- Table `RISO`.`tb_conceito`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RISO`.`tb_termo` ;
+DROP TABLE IF EXISTS `RISO`.`tb_conceito` ;
 
-CREATE TABLE IF NOT EXISTS `RISO`.`tb_termo` (
+CREATE TABLE IF NOT EXISTS `RISO`.`tb_conceito` (
   `id` INT(11) NOT NULL,
-  `nome` VARCHAR(100) NOT NULL,
+  `termo` VARCHAR(100) NOT NULL,
   `descricao` VARCHAR(100) NULL,
   `contexto` VARCHAR(100) NULL,
   PRIMARY KEY (`id`));
@@ -31,18 +31,18 @@ CREATE TABLE IF NOT EXISTS `RISO`.`tb_termo` (
 DROP TABLE IF EXISTS `RISO`.`tb_conceito` ;
 
 CREATE TABLE IF NOT EXISTS `RISO`.`tb_conceito` (
-  `id_termo_principal` INT(11) NOT NULL,
-  `id_termo_secundario` INT(11) NOT NULL,
+  `id_conceito_principal` INT(11) NOT NULL,
+  `id_conceito_secundario` INT(11) NOT NULL,
   `relacao` VARCHAR(100) NOT NULL,
-  INDEX `fk_tb_termo_idx` (`id_termo_principal` ASC),
-  CONSTRAINT `fk_tb_termo_principal`
-    FOREIGN KEY (`id_termo_principal`)
-    REFERENCES `INSA`.`tb_termo` (`id`)
+  INDEX `fk_tb_conceito_idx` (`id_conceito_principal` ASC),
+  CONSTRAINT `fk_tb_conceito_principal`
+    FOREIGN KEY (`id_conceito_principal`)
+    REFERENCES `INSA`.`tb_conceito` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION),
-  CONSTRAINT `fk_tb_termo_secundario`
-    FOREIGN KEY (`id_termo_secundario`)
-    REFERENCES `INSA`.`tb_termo` (`id`)
+  CONSTRAINT `fk_tb_conceito_secundario`
+    FOREIGN KEY (`id_conceito_secundario`)
+    REFERENCES `INSA`.`tb_conceito` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
@@ -59,20 +59,20 @@ CREATE TABLE IF NOT EXISTS `RISO`.`tb_documento` (
   PRIMARY KEY (`id`));
 
 -- -----------------------------------------------------
--- Table `RISO`.`tb_termo_documento`
+-- Table `RISO`.`tb_conceito_documento`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `RISO`.`tb_termo_documento` ;
+DROP TABLE IF EXISTS `RISO`.`tb_conceito_documento` ;
 
-CREATE TABLE IF NOT EXISTS `RISO`.`tb_termo_documento` (
-  `id_termo` INT(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `RISO`.`tb_conceito_documento` (
+  `id_conceito` INT(11) NOT NULL,
   `id_documento` INT(11) NOT NULL,
   INDEX `fk_tb_documento_idx` (`id_documento` ASC),
-  CONSTRAINT `fk_tb_termo_documento`
-    FOREIGN KEY (`id_termo`)
-    REFERENCES `INSA`.`tb_termo` (`id`)
+  CONSTRAINT `fk_tb_conceito_documento`
+    FOREIGN KEY (`id_conceito`)
+    REFERENCES `INSA`.`tb_conceito` (`id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_tb_termo_documento_2`
+  CONSTRAINT `fk_tb_conceito_documento_2`
     FOREIGN KEY (`id_documento`)
     REFERENCES `INSA`.`tb_documento` (`id`)
     ON DELETE NO ACTION
--- db-scripts/inicial_script.py
+++ db-scripts/inicial_script.py
@@ -7,31 +7,38 @@ import os, sys
 from unicodedata import normalize
 from datetime import datetime
 
-try:
-	conn = MySQLdb.connect(read_default_group='RISO')
-	cursor = conn.cursor()
-	for line in open("db_riso.sql").read().split(';\n'):
-		if(line != ""):
-			cursor.execute(line)
-finally:
-	cursor.close()
-	conn.close()
 
-def execute_many_BD(insert,values):
-	conn = MySQLdb.connect(read_default_group='RISO',db="RISO")
-	cursor = conn.cursor()
-	try:
-		cursor.executemany(insert, values)
-		conn.commit()
-	except MySQLdb.Error as e:
-		print "Error", e
-		conn.rollback()
 
-	conn.close()
+def insertConcept(term, desc, ctxt, file):
+	for hc in ctxt:
+		limit = len(hc) - 1
+		hc = hc[1:limit]
+
+def concepts(data_file):
+	term = ""
+	desc = ""
+	ctxt = ""
+	for line in data_file:
+		limit = len(line) - 3
+		if "\"Term\"" in line:
+			term = line[8:limit]
+		elif "\"Description\"" in line:
+			desc = line[15:limit]
+		elif "\"hasContext\"" in line:
+			ctxt = line[15:limit+1]
+			ctxt = ctxt.split(", ")
+		else:
+			if term == "":
+				insertConcept(term, desc, ctxt, file)
+				term = ""
+				desc = ""
+				ctxt = ""
+
+def 
 
 # Open a file
 path = "../data/"
 dirs = os.listdir( path )
 for file in dirs:
-   with open(file) as data_file:
-	_terms = json.load(data_file)
+	with open(path+file) as data_file:
+		concepts(data_file) #Put concepts into DB
