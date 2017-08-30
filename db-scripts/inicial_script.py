d939756c6fa68ddf892626601d9426509fe902ca
igormeira <igor.meira@gmail.com>
2017-08-30 16:53:05 -0300

    Colocando dados no BD

--- db-scripts/inicial_script.py
+++ db-scripts/inicial_script.py
@@ -7,7 +7,27 @@ import os, sys
 from unicodedata import normalize
 from datetime import datetime
 
+try:
+	conn = MySQLdb.connect(read_default_group='RISO')
+	cursor = conn.cursor()
+	for line in open("db_riso.sql").read().split(';\n'):
+		if(line != ""):
+			cursor.execute(line)
+finally:
+	cursor.close()
+	conn.close()
 
+def execute_many_BD(insert,values):
+	conn = MySQLdb.connect(read_default_group='RISO',db="RISO")
+	cursor = conn.cursor()
+	try:
+		cursor.executemany(insert, values)
+		conn.commit()
+	except MySQLdb.Error as e:
+		print "Error", e
+		conn.rollback()
+
+	conn.close()
 
 def insertConcept(term, desc, ctxt, file):
 	for hc in ctxt:
@@ -34,7 +54,7 @@ def concepts(data_file):
 				desc = ""
 				ctxt = ""
 
-def 
+def
 
 # Open a file
 path = "../data/"
